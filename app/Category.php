93c43cfd52ee86b58a1ff9553a15a60de444669e
Om Gurung <suman.gurung@hotmail.com>
2016-09-18 14:11:24 +0100

    add name field to fillable

--- app/Category.php
+++ app/Category.php
@@ -8,6 +8,8 @@ class Category extends Node
 {
     protected $table = 'categories';
 
+    protected $fillable = ['name'];
+
     public function products() {
         return $this->belongsToMany('App\Product', "category_product")->withTimestamps();
     }
